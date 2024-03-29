676b98480a365939c64517b08ab5daab7e4884dc
Ftphacker <59247929+Ftphacker@users.noreply.github.com>
2024-03-29 19:47:09 +0300

    Delete Scripts directory

--- Scripts/installation.sh
+++
@@ -1,6 +0,0 @@
-sudo apt update
-sudo apt -y install python3-pip
-sudo pip install ansible
-sudo apt-get install sshpass
-
-
--- Scripts/ssh_operations1.sh
+++
@@ -1,6 +0,0 @@
-ssh-keygen -t rsa
-sudo cp /home/vagrant/.ssh/id_rsa.pub /vagrant/.ssh/id_rsa.pub
-chmod 700 /home/vagrant/.ssh
-chmod 600 /home/vagrant/.ssh/*
-cat /vagrant/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
-chown -R vagrant:vagrant /home/vagrant/.ssh
